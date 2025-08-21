91305de14521b4e3765b654a7a0b7817ec03e86a
Satyam382003 <140716707+Satyam382003@users.noreply.github.com>
2025-08-21 22:12:25 +0530

    Update axiosInstances.js

--- frontend/src/utils/axiosInstances.js
+++ frontend/src/utils/axiosInstances.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 import { BASE_URL } from './constants';
 
 const axiosInstances = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://note-app-application.onrender.com"
     timeout: 10000,
+    withCredentials: true,
     headers:{
         "Content-Type": "application/json",
     },
 });
+
+ export const loginUser = (data) => API.post("/login", data);
+    export const signupUser = (data) => API.post("/signup", data);
+    export const fetchNotes = () => API.get("/api/notes");
 
 axiosInstances.interceptors.request.use(
     (config) => {
