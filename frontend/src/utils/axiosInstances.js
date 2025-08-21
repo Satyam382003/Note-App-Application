77bbe6b886f27ae71defb16193326ccddccbf108
Satyam382003 <140716707+Satyam382003@users.noreply.github.com>
2025-08-21 23:41:47 +0530

    Update axiosInstances.js

--- frontend/src/utils/axiosInstances.js
+++ frontend/src/utils/axiosInstances.js
@@ -9,10 +9,18 @@ const axiosInstances = axios.create({
         "Content-Type": "application/json",
     },
 });
+// User APIs
+export const signupUser = (data) => API.post("/create-account", data);
+export const loginUser = (data) => API.post("/login", data);
+export const getUser = () => API.get("/get-user");
 
- export const loginUser = (data) => API.post("/login", data);
-    export const signupUser = (data) => API.post("/signup", data);
-    export const fetchNotes = () => API.get("/api/notes");
+// Notes APIs
+export const addNote = (data) => API.post("/add-note", data);
+export const getAllNotes = () => API.get("/get-all-notes");
+export const editNote = (id, data) => API.put(`/edit-note/${id}`, data);
+export const deleteNote = (id) => API.delete(`/delete-note/${id}`);
+export const updateNotePinned = (id, data) => API.put(`/update-note-pinned/${id}`, data);
+export const searchNotes = (query) => API.get(`/search-notes?query=${query}`);
 
 axiosInstances.interceptors.request.use(
     (config) => {
